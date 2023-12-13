2800539dec5c7c20e198c8449d4fb6774d61fa60
Dave Powers <djpowers89@gmail.com>
2023-12-12 19:18:42 -0500

    Implement updating and creating product records

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,24 @@
 class ProductsController < ApplicationController
   def new
+    @product = Product.new
+  end
+
+  def create
+    if product_params[:barcode].present?
+      debugger
+      @product = Product.find_by(barcode: product_params[:barcode])
+      if @product.update(product_params.compact_blank)
+        redirect_to new_product_path
+      else
+        render :new
+      end
+    else
+      if Product.create(product_params.compact_blank)
+        redirect_to new_product_path
+      else
+        render :new
+      end
+    end
   end
 
   def show
@@ -8,5 +27,11 @@ class ProductsController < ApplicationController
     else
       head :not_found
     end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:barcode, :name, :description, :weight, :bucket)
   end
 end
